87e3c7e4ad7eb3a94febff592871b407a1b5d471
Justin Miller <justinamiller1023@gmail.com>
2024-05-06 22:18:03 -0600

    added hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unescaped-entities */
+
+function Hero() {
+    return (
+        <div>
+            <div className="space"></div>
+            <h1 className="title">Kendrick vs Drake Timeline</h1>
+            <div className="space"></div>
+
+            <h2>March 22, 2024</h2>
+            <h3>In “Like That,” Kendrick Lamar dismisses the notion that he’s in a “Big Three” with Drake and J. Cole, rapping, “It’s just big me.” He also compares himself and Drake to Prince and Michael Jackson, respectively, rapping, “Prince outlived Mike Jack.”</h3>
+
+            <h2>April 13, 2024</h2>
+            <h3>Drake drops "Push Ups" in which he calls Kendrick small and implies that he isn't even a top three rapper.</h3>
+
+            <h2>April 19, 2024</h2>
+            <h3>Drake drops "Taylor Made Freestyle" in which he uses AI generate performances from Kendrick's idols, Snoop and Tupac, to roast him. He insinuates that Kendrick won't respond during the week Taylor Swift drops her album.</h3>
+
+            <h2>April 30, 2024</h2>
+            <h3>Kendrick gets aggressive and personal with Drake in "Euphoria". He implies Drake isn't black and shouldn't say the N-word. He calls Drake a pussy by referencing Drake's cat's name repeatedly.</h3>
+
+            <h2>May 3, 2024</h2>
+            <h3>Kendrick double drops with “6:16 in LA." He implies Drake's possey isn't loyal.</h3>
+
+            <h2>May 3, 2024</h2>
+            <h3>Drake responds same day with "Family Matters." He suggests Kendrick's Fiance isn't faithful and reference domestic violence alegations between the two.</h3>
+
+            <h2>May 4, 2024</h2>
+            <h3>Kendrick responds with “Meet the Grahams” where he suggests Drake has a secret daughter.</h3>
+
+            <h2>May 4, 2024</h2>
+            <h3>Kendrick drops “Not Like Us” same day where he suggests Drake is a pedophile and is involved with underage women.</h3>
+
+            <h2>May 5, 2024</h2>
+            <h3>Drake responds with “The Heart Part 6” where he revisits AI and suggests his team planted the secret daughter rumor and Kendrick was too dumb to fact check it.</h3>
+        </div>
+    )
+}
+
+export default Hero
